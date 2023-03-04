dbb5407d01a8071ac442b40552689525dab77226
MinJeong Kim <alswjd980512@naver.com>
2023-03-04 21:02:40 +0900

    [Gold V] Title: 탑, Time: 780 ms, Memory: 106316 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,62 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
-import java.util.StringTokenizer;
+
 
 /*
- * 1. 현재 들어온 값보다 작으면 pop()
- * 2. 현재 들어온 값보다 같거나 크면 출력
- * 3. 현재 값 push
+ * [풀이]
+ * n<=500,000 이므로 for문을 이용하기엔 시간이 너무 많이 걸린다.
+ * 자신에게 가장 가까우면서 큰 탑을 찾아야하므로 스택을 사용한다.
+ * 앞부터 자신을 스택에 넣기전에 자신보다 큰 탑이 있는지 확인한 후 자신을 넣는다. 
+ * 자신보다 작으면 자신에게 막혀 뒤에 탑들의 수신을 못받으므로 stack에서 뺀다. 
  */
-class Node{
-	int value;
-	int idx;
-	
-	Node(int value, int idx) {
-		this.value = value;
-		this.idx = idx;
-	}
-}
-
-public class Main {
+//Solution_2115_김민정
+class Main
+{
 	static int n;
-	static StringBuilder sb=new StringBuilder();
-
-	public static void main(String[] args) throws IOException{
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(in.readLine());
+	static int[] top;
+	
+	public static void main(String args[]) throws Exception
+	{
+		input();
 		
-		StringTokenizer tokens = new StringTokenizer(in.readLine(), " ");
-		int idx=0; // 인덱스
-		Stack<Node> stack = new Stack<Node>();
-		Node cur=null; // 현재 stack의 top 값
-		int curTop; // 현재 값
-		while(tokens.hasMoreTokens()) {
-			idx++;
-			curTop=Integer.parseInt(tokens.nextToken());
-			if(stack.isEmpty()) { // stack에 값이 없으면 수신 받을 수 없음
-				sb.append("0 ");
-			}else {
-				// stack에 현재 값보다 작으면 pop. 크면 해당 값의 인덱스 출력
-				while(!stack.isEmpty()) {
-					cur=stack.peek();
-					if(cur.value >= curTop) break;
-					stack.pop();
-				}
-				
-				if(stack.isEmpty()) {
-					sb.append("0 ");
-				}else {
-					sb.append(cur.idx);
-					sb.append(" ");
-				}
-				
+		StringBuilder sb=new StringBuilder();
+		Stack<Integer> stack=new Stack<Integer>();
+		int curIdx=0, save;
+		while(curIdx<n) {
+			if(stack.isEmpty()) { // stack에 없으면 신호를 받을 탑이 없다는 것 
+				stack.add(curIdx++);
+				sb.append(0+" ");
+				continue;
 			}
-			stack.push(new Node(curTop, idx)); // 현재 값 push
+			
+			save=stack.peek();
+			if(top[save]<top[curIdx]) { // 현재 스택에서나 온 값이 들어갈 탑보다 작은 경우 
+				stack.pop();
+				continue;
+			}
+			
+			// 신호를 받을 탑을 나온 경
+			stack.add(curIdx++);
+			sb.append((save+1)+" ");
 		}
 		
-		System.out.println(sb.toString());
+		System.out.println(sb);
 	}
 
+	public static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n=Integer.parseInt(br.readLine());
+		top=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+	}
 }
